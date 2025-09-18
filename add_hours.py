ae5ed2b40c57b9b5ab733a1fc580e0b2d8127a95
sasha (mint221) <flarexandra@gmail.com>
2025-09-18 18:53:01 +0200

    feat: editing committer date too

--- add_hours.py
+++ add_hours.py
@@ -1,11 +1,14 @@
 def handle(commit):
     "Reset the timezone of all commits."
-    date_str = commit.author_date.decode('utf-8')
-    [seconds_str, timezone] = date_str.split()
+    author_date_str = commit.author_date.decode('utf-8')
+    [author_seconds_str, timezone] = author_date_str.split()
+
     # print(int(seconds_str))  # "Debugging"
     # print(timezone)  # "Debugging"
-    new_seconds = int(seconds_str) + 3600 * (3)
-    new_date = f"{new_seconds} {timezone}"
-    commit.author_date = new_date.encode('utf-8')
+    new_seconds = int(author_seconds_str) + 3600 * (3)
+    new_date_str = f"{new_seconds} {timezone}"
+    new_date = new_date_str.encode('utf-8')
+    commit.author_date = new_date
+    commit.committer_date = new_date
 
 handle(commit)
